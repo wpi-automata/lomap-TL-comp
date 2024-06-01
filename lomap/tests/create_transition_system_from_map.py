39bcfcced5583f1e511fff1a15ecca0f30d54047
Taylor Bergeron <tberg1234@gmail.com>
2024-05-31 20:40:52 -0400

    symbol graph created

--- lomap/tests/create_transition_system_from_map.py
+++ lomap/tests/create_transition_system_from_map.py
@@ -7,6 +7,62 @@ from test_map_word_accepted_randomized_occupancy_grid import *
 from lomap.algorithms.product import ts_times_buchi
 from lomap.classes import Buchi, Ts
 
+# Load map, start and goal point.
+def load_symbol_map(file_path):
+    grid = []
+    start = [0, 0]
+    goal = [0, 0]
+    # Load from the file
+    with open(file_path, 'r') as map_file:
+        reader = csv.reader(map_file)
+        for i, row in enumerate(reader):
+            # load start and goal point
+            if i == 0:
+                start[0] = int(row[1])
+                start[1] = int(row[2])
+            elif i == 1:
+                goal[0] = int(row[1])
+                goal[1] = int(row[2])
+            # load the map
+            else:
+                parsed_row = [col for col in row]
+                grid.append(parsed_row)
+    return grid, start, goal
+
+def assign_props(grid):
+    props = dict()
+    reduced = list(set(i for j in grid for i in j))
+    reduced.remove("0")
+    reduced.sort()
+
+    single_values=[x for x in reduced if len(x)==1]
+
+    for i in range(len(single_values)):
+        props[single_values[i]] = 2**i
+
+    return props
+
+def create_numerical_grid(props, symbol_grid):
+
+    grid = copy.deepcopy(symbol_grid)
+
+    for row in range(len(symbol_grid)):
+        for col in range(len(symbol_grid[0])):
+            val = symbol_grid[row][col]
+            if val != "0": #empty
+                if val in list(props.keys()):
+                    grid[row][col] = props.get(val)
+                elif len(val) > 1:
+                    sum = 0
+                    for letter in val:
+                        sum += props.get(letter)
+                    grid[row][col] = sum
+                else:
+                    print("ERROR parsing grid from symbols to binary numerical translation")
+    
+    return grid
+
+
 def create_transitions(G, edges, nodes):
 
     spl = dict(nx.all_pairs_shortest_path_length(G))
@@ -120,11 +176,40 @@ def case_1(node, node_outgoing_labels, new_transition_dict):
 def case_2(simplfied_node_rep, node_outgoing_labels):
     for key in node_outgoing_labels.keys():
             if simplfied_node_rep in node_outgoing_labels.get(key):
-                node_outgoing_labels.get(key).remove(simplfied_node_rep)               
+                node_outgoing_labels.get(key).remove(simplfied_node_rep)      
+
+def convert_edges_alphabetical(labels, props, edges):
+
+    num_to_alpha_prop = {v: k for k, v in props.items()}
+
+    #TODO: remove 0? make empty set???
+    #FIXMES: will need to replace 0 to get correct product
+
+    for edge in edges:
+        pi = labels.get(str(edge))
+        for i in range(len(pi)):
+            if int(pi[i]) != 0:
+                pi[i] = num_to_alpha_prop.get(int(pi[i]))
+
+        if int(edge[0])!=0:
+            edge[0] = num_to_alpha_prop.get(int(edge[0]))
+        if int(edge[1])!=0:
+            edge[1] = num_to_alpha_prop.get(int(edge[1]))
+        edge.append({'pi':pi})
+
+    return edges
 
 
 def main():
-    grid, start, goal = load_map('maps/map_multiple_symbols_BCD.csv')
+    '''
+    Map must only contain 0s or alphabetical values
+    '''
+    symbol_grid, start, goal = load_symbol_map('maps/map_multiple_alpha_symbols_simple.csv')
+    props = assign_props(symbol_grid)
+    grid = create_numerical_grid(props, symbol_grid)
+
+    print(f"replaced grid: {grid}")
+
     # draw_path(grid, start, goal, [], 'Map')
     clusters = create_clusters(np.asarray(grid))
     print(f"Clusters: {clusters}")
@@ -136,17 +221,19 @@ def main():
     print(f"Reversed Edges: {reversed_edges}")
     edges.extend(reversed_edges)
 
-    G = nx.DiGraph()
-    G.add_edges_from(edges)
+    intermediate_G = nx.DiGraph()
+    intermediate_G.add_edges_from(edges)
     
-    labels = create_transitions(G, edges, list(unique_clusters.keys()))
+    labels = create_transitions(intermediate_G, edges, list(unique_clusters.keys()))
     labels = prune_labels(list(unique_clusters.keys()), labels)
 
-    G.remove_edges_from(edges)
+    G = nx.DiGraph()
 
-    for edge in edges:
-        edge.append({'pi':labels.get(str(edge))})
-    G.add_edges_from(edges)
+    #TODO: determine how to handle multiple nodes with same label
+    #FIXME: this will incorrectly override duplicate nodes, making them seem like the same node. For all node.uuid, the uuid is erased
+    alphabetical_edges = convert_edges_alphabetical(labels, props, edges)
+
+    G.add_edges_from(alphabetical_edges)
 
     draw_graph(G)
 
@@ -156,7 +243,7 @@ def main():
     ts.g = G
     # ts.init[(1, 1)] = 1
 
-    spec = 'G (F a && F g && !e)'
+    spec = 'G a'
     buchi = Buchi()
     buchi.from_formula(spec)
     print('Created Buchi automaton of size', buchi.size())
